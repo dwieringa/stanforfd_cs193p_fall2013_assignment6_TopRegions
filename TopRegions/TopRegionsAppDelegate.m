87be307e87cb69ebc1c6ff85ab51e6d81b310367
Dave Wieringa <dwjm@userwise.com>
2014-04-23 15:26:17 -0400

    add periodic foreground fetches

--- TopRegions/TopRegionsAppDelegate.m
+++ TopRegions/TopRegionsAppDelegate.m
@@ -14,7 +14,17 @@
 @interface TopRegionsAppDelegate() <NSURLSessionDownloadDelegate>
 @property (strong, nonatomic) UIManagedDocument *document;
 @property (strong, nonatomic) NSManagedObjectContext *photoDatabaseContext;
+@property (strong, nonatomic) NSTimer *flickrForegroundFetchTimer;
 @end
+
+// name of the Flickr fetching background download session
+#define FLICKR_FETCH @"Flickr Just Uploaded Fetch"
+
+// how often (in seconds) we fetch new photos if we are in the foreground
+#define FOREGROUND_FLICKR_FETCH_INTERVAL (20*60)
+
+// how long we'll wait for a Flickr fetch to return when we're in the background
+#define BACKGROUND_FLICKR_FETCH_TIMEOUT (10)
 
 @implementation TopRegionsAppDelegate
 
@@ -50,13 +60,13 @@
 {
     if (self.document.documentState == UIDocumentStateNormal) {
         self.photoDatabaseContext = self.document.managedObjectContext;
-        [self fetchPhotos];
+        [self startPhotoFetch];
     } else {
         NSLog(@"trouble in documentIsReady. Check/handle document states");
     }
 }
 
-- (void)fetchPhotos
+- (void)startPhotoFetch
 {
     [UIApplication sharedApplication].networkActivityIndicatorVisible = YES; // start the spinner
     NSURL *url = [FlickrFetcher URLforRecentGeoreferencedPhotos];
@@ -75,9 +85,23 @@
     });
 }
 
+- (void)startPhotoFetch:(NSTimer *)timer
+{
+    [self startPhotoFetch];
+}
+
 - (void)setPhotoDatabaseContext:(NSManagedObjectContext *)photoDatabaseContext
 {
     _photoDatabaseContext = photoDatabaseContext;
+    
+    if (self.photoDatabaseContext) {
+        // this timer will fire only when we are in the foreground
+        self.flickrForegroundFetchTimer = [NSTimer scheduledTimerWithTimeInterval:FOREGROUND_FLICKR_FETCH_INTERVAL
+                                                                           target:self
+                                                                         selector:@selector(startPhotoFetch:)
+                                                                         userInfo:nil
+                                                                          repeats:YES];
+    }
     
     NSDictionary *userInfo = self.photoDatabaseContext ? @{ PhotoDatabaseAvailabilityContext : self.photoDatabaseContext } : nil;
     [[NSNotificationCenter defaultCenter] postNotificationName:PhotoDatabaseAvailabilityNotification
